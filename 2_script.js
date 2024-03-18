e59566c859b6d9886e3b4d617c98feef34bca4b6
DiegoValdivieso22 <159164145+DiegoValdivieso22@users.noreply.github.com>
2024-03-18 15:07:16 +0000

    edit

---
+++ 2_script.js
@@ -0,0 +1,4 @@
+function doMathClicked ()
+// this function calculates area and perimiter of a rectangle
+document.getElementById ( 'area-math'' ).innerHTML = '8 * 12 = ' + (8 * 12) 
+document.getElementById ( 'perimiter-math' ).innerHTML = '(8 + 12) * 2  + ((8 + 12) * 2)
--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+// Copyright (c) 2022 Ms Raffin All rights reserved
+//
+// Created by: Ms Raffin
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('add-math').innerHTML = '8 + 6 =  ' + (8 + 6)
+  document.getElementById('subtract-math').innerHTML = '22 - 8 =  ' + (22 - 8)
+  document.getElementById('multiply-math').innerHTML = '5 + 5 * 5 = ' + (5 + 5 * 5)
+  document.getElementById('divide-math').innerHTML = '(100 / 10) + 5 = ' + ((100 / 10) + 5)
+  document.getElementById('exponent-math').innerHTML = '10 + 5 ** 3 = ' + (10 + 5 ** 3)
+  document.getElementById('square-root-math').innerHTML = '√(100000) = ' + Math.sqrt(100000)
+}
